04fc3eb5e50b4414be3e2f1b5540a6c80bc9c3ad
shykulad <de.shykula@gmail.com>
2018-02-23 16:42:11 +0200

    Added styles, normalize.css and some refactoring

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@
  * Returns a random integer between min (inclusive) and max (inclusive)
  * Using Math.round() will give you a non-uniform distribution!
  */
-document.getElementById("conf").addEventListener("click", getRandomInt);
+document.getElementById("btnSbm").addEventListener("click", getRandomInt);
 
 function getRandomInt() {
     var min = document.getElementById("firstNumber").value;
     var max = document.getElementById("secondNumber").value;
-    var res = Math.floor(Math.random() * (max - min + 1)) + min;
-    alert(res);
+    var result = Math.floor(Math.random() * (max - min + 1)) + min;
+    alert("Your random number is " + result);
 }
